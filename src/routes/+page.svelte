28dfc41c6bf88389720e73063f8c9604f0680932
JokeNeverSoke <zengjoseph@hotmail.com>
2023-02-20 10:10:50 +0000

    feat(dev): add libs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	console.log('HI');
+</script>
+
+<div class="mx-auto text-2xl container ">
+	<h1>Welcome to <kbd>KBD</kbd></h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
